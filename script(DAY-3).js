d3e276229ed08b2995b8ade49f93ba8a7db09712
Tharunkumar <tharunkumar10406@gmail.com>
2025-06-20 23:38:06 +0530

    DAY-4 Commit updation\

--- script(DAY-3).js
+++ script(DAY-3).js
@@ -93,24 +93,26 @@ function func() {
     console.log(ele.innerHTML);
     let body1 = document.getElementById("dom");
     console.log(body1);
+}
+function funct() {
+    let body = document.getElementById("dom1");
+    console.log(body);
     let newe = document.createElement("h2");
     newe.innerText = "Hi,This is Tharunkumar here!";
 
 
-    let newele2 = document.createElement("h2");
-    newele1.innerText = "I am pursuing my B.E in Electronics and Instrumentation Enginnering at Kongu Engineering College,Perundurai,Erode.";
-    setTimeout(() => { body.appendChild(newele2); }, 1000);
-    setTimeout(() => { body.removeChild(newele2); }, 5000);
+    let newele = document.createElement("h2");
+    newele.innerText = "I am pursuing my B.E in Electronics and Instrumentation Enginnering at Kongu Engineering College,Perundurai,Erode.";
+    setTimeout(() => { body.appendChild(newele); }, 1000);
+    setTimeout(() => { body.removeChild(newele); }, 5000);
 
     let newele1 = document.createElement("h2")
     newele1.innerText = "Seeking for an InternShip";
     setTimeout(() => { body.appendChild(newele1); }, 5000);
     setTimeout(() => { body.removeChild(newele1); }, 10000);
 
-    let obj1 = document.getElementById("event");
-    obj1.addEventListener("click", () => {
-        obj1.innerText = "clicked";
+    let obj = document.getElementById("event");
+    obj.addEventListener("click", () => {
+        obj.innerText = "clicked";
     });
-
-
 }
